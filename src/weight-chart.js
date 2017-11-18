54c50806bc1bf26141b24e378c30f6cf71a2c9c6
Creechie <chazzimator@gmail.com>
2017-11-18 17:35:59 +0000

    JS documentation

--- src/weight-chart.js
+++ src/weight-chart.js
@@ -1,6 +1,13 @@
-function loadDataFromCSV() {
-
+/** 
+ * Returns an array of all items with a specifed index from the user-diary.json. 
+ * @param {!number} index 0 = date string | 1 = weight | 2 = calories
+ *//**
+ * Returns an array of all items with a specifed index from the user-diary.json. 
+ * @param {!string} label "date" | "weight" | "calories"
+ * */
+function loadDataFromDiary(index) {
 }
+
 
 $(function () {
     let myChart = document.getElementById('weightChart').getContext('2d');
@@ -23,32 +30,8 @@ $(function () {
             ],
             datasets: [{
                 label: 'Population',
-                data: [
-                    7074265,
-                    1124600,
-                    781700,
-                    575400,
-                    553700,
-                    541300,
-                    534300,
-                    522100,
-                    488400,
-                    484600,
-                    454200
-                ],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.6)',
-                    'rgba(54, 162, 235, 0.6)',
-                    'rgba(255, 206, 86, 0.6)',
-                    'rgba(75, 192, 192, 0.6)',
-                    'rgba(153, 102, 255, 0.6)',
-                    'rgba(255, 159, 64, 0.6)',
-                    'rgba(145, 255, 140, 0.6)',
-                    'rgba(235, 123, 255, 0.6)',
-                    'rgba(219, 255, 123, 0.6)',
-                    'rgba(130, 142, 255, 0.6)',
-                    'rgba(130, 255, 204, 0.6)'
-                ]
+                data: [ loadDataFromDiary() ],
+                backgroundColor: ['rgba(255, 99, 132, 0.6)']
             }]
         },
         options: {}
