3b1846cd149b0376797a71b138c3334c41b00ae8
Creechie <chazzimator@gmail.com>
2017-11-19 22:18:46 +0000

    Made to run on a server

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ $(function () {
     $('#btn-sidebar-toggle').click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
-        // $(".sidebar-brand h1").toggleClass("visible");
     });
 });
--- src/weight-chart.js
+++ src/weight-chart.js
@@ -1,21 +1,87 @@
-/** 
- * Returns an array of all items from the specifed index from the user-diary.json. 
- * @param {(number|string)} index (0 | "date") | (1 | "weight") | (2 | "calories")
- * @returns {(number[]|string[])} All items from the specifed index from the user-diary.json
- * */
-function loadDataFromDiary(index) {}
+function loadJSON(callback) {
+   
 
+
+
+
+    // var xobj = new XMLHttpRequest();
+    // xobj.open('GET', url);
+
+    // xobj.responseType = 'json';
+    // xobj.send();
+
+    // xobj.onload = function() {
+    //     var data = xobj.response;
+    // };
+
+
+
+    // xobj.overrideMimeType("application/json");
+    // xobj.open('GET', url);
+    // xobj.onreadystatechange = function() {
+    //     if (xobj.readyState == 4 && xobj.status == "200") {
+    //         callback(xobj.responseText);
+    //     }
+    // };
+    // xobj.send(null);
+}
+
+/**
+ * Returns the value with specifed index and label from user-diary.json.
+ * @param {number} index The index of the item
+ * @param {(number|string)} label (0 | "date") | (1 | "weight") | (2 | "calories")
+ * @returns {(number|string)} Value with specifed index and label from user-diary.json.
+ * */
+function getItemFromDiary(index, label) {
+
+}
+
+/** 
+ * Returns all items with the specifed label from the user-diary.json. 
+ * @param {(number|string)} label (0 | "date") | (1 | "weight") | (2 | "calories")
+ * @returns {(number[]|string[])} All items with the specifed label from the user-diary.json
+ * */
+function getAllFromDiary(label) {
+
+}
 
 $(function () {
+
+    // let url = 'http://localhost:8080/user-diary.json';
+    
+    // var jsonResult;
+
+    // console.log('Requesting JSON');
+    // $.getJSON(url, function(data) {
+    //     console.log('Success\n');
+    //     console.log(data);
+    // })
+    // .fail(function(data, textStatus, error) {
+    //     if (!error) console.error("JSON request failed.\n-Status: " + textStatus + "\n-Error: Violating Same-origin policy");
+    //     else console.error("JSON request failed.\n-Status: " + textStatus + "\n-Error: " + error);
+    // })
+    // .always(function(data) {
+    //     console.log("JSON request complete");
+    // });
+
+
+
+
+    //// Parse JSON into object
+    // loadJSON(function(response) {
+    //     var diary = JSON.parse(response);
+    //     console.log(diary);
+    // });
+
     let myChart = document.getElementById('weightChart').getContext('2d');
 
     let massPopChart = new Chart(weightChart, {
         type: 'line', //bar, horizontalBar, pie, line, doughnut, radar, polarArea
         data: {
-            labels: loadDataFromDiary("date"),
+            labels: getAllFromDiary("date"),
             datasets: [{
                 label: 'Weight',
-                data: loadDataFromDiary("weight")
+                data: getAllFromDiary("weight")
             }]
         },
         options: {}
