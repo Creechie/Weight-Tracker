cedb63a6f7c8522f680e53cb3373ec02474f6640
Creechie <chazzimator@gmail.com>
2017-11-28 21:47:57 +0000

    Cleaned up old code

--- server.js
+++ server.js
@@ -7,12 +7,6 @@ server.use('/assets', express.static('assets'));
 server.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
-server.get('/contact', function(req,res) {
-    res.send('Hello again');
-});
 
-server.get('/profile/:id', function(req,res) {
-    res.send('You requested a profile with the id of ' + req.params.id); 
-});
 
 server.listen(8080);
