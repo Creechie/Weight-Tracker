1e943bd36aecff06945ac9173ec593a500fbe29a
Creechie <chazzimator@gmail.com>
2017-11-30 22:51:34 +0000

    Added calories per day to diary data structure

--- assets/js/weight-chart.js
+++ assets/js/weight-chart.js
@@ -1,11 +1,12 @@
 $(function () {
     $.getJSON('http://127.0.0.1:8080/assets/data/user-diary.json', function (data) {
 
-        var diary = {dates:[], weights:[]};
+        var diary = {dates:[], weights:[], calories:[]};
         var i = 0;
         data.diary.forEach(entry => {
             diary.dates[i] = entry.date;
             diary.weights[i] = entry.weight;
+            diary.calories[i] = entry.calories;
             i++;
         });
 
@@ -20,25 +21,24 @@ $(function () {
                     data: diary.weights,
 
                     // Chart options
-                    backgroundColor: 'rgba(79, 129, 189, 0.7)',
+                    backgroundColor: 'rgba(80, 130, 190, 0.7)',
                     fill: false,
                     spanGaps: true,
                     // Line options
                     lineTension: 0.1,
-                    borderColor: 'rgb(79, 129, 189)',
+                    borderColor: 'rgb(80, 130, 190)',
                     borderWidth: 3,
                     // Point options
                     pointStyle: 'dot',
                     pointRadius: 0,
                     pointHoverRadius: 5,
                     pointHitRadius: 10,
-                    pointBorderColor: 'rgba(0, 0, 0, 0.5)',
+                    pointBorderColor: 'rgba(0, 50, 100, 0.5)',
                     pointBorderWidth: 2,
                 }]
             },
             options: {
-                
-                
+
             }
         });
     });
