67ddb0cd97e8bd11d6769fd728025c294af75507
Creechie <charlie.creech95@gmail.com>
2018-03-23 21:36:43 +0000

    Added input validation to fields

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,16 @@ $(function () {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
-});
+});   
+
+// Uses regular expression to only accept numerical input
+function validate(evt) {
+    var theEvent = evt || window.event;
+    var key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode( key );
+    var regex = /[0-9]|\./;
+    if( !regex.test(key)) {
+        theEvent.returnValue = false;
+        if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+}
