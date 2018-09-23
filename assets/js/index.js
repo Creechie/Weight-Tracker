1307033c27c45f3d300a5deb21e9335845ce37ed
Creechie <charlie.creech95@gmail.com>
2018-09-23 07:53:51 +0100

    Linked ADD request to frontend Stripped down index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,42 @@
 $(function () {
-    $('#btn-sidebar-toggle').click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
+    // $('#btn-sidebar-toggle').click(function(e) {
+    //     e.preventDefault();
+    //     $("#wrapper").toggleClass("toggled");
+    // });
+    $('#newEntry').click(function (err) {
+        err.preventDefault();
+        $("#inputBox").fadeIn(function () {
+            // Animation complete
+        });
     });
-});   
+    $('#submit').click(function (err) {
+        err.preventDefault();
+        submit();
+    });
+});
 
-// Uses regular expression to only accept numerical input
-function validate(evt) {
-    var theEvent = evt || window.event;
-    var key = theEvent.keyCode || theEvent.which;
-    key = String.fromCharCode( key );
-    var regex = /[0-9]|\./;
-    // Full string validation = /^[1-9][0-9]*([0-9]*|\.[0-9]+$)+/;
- 
-    if( !regex.test(key)) {
-        theEvent.returnValue = false;
-        if(theEvent.preventDefault) theEvent.preventDefault();
-    }
+function submit() {
+    var username = $("#username").text();
+    var weight = $(":input#weight").val();
+    var kcal = $(":input#calories").val();
+    var date = today();
+
+    saveJSON(username, date, weight, kcal);
+}
+
+function saveJSON(user, date, weight, kcal) {
+    var req = '/' + user + '/add/' + date + '/' + weight + '/' + kcal;
+    $.getJSON(req);
+}
+
+function today() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) dd = '0' + dd
+    if (mm < 10) mm = '0' + mm
+    today = dd + '-' + mm + '-' + yyyy;
+    return today;
 }
